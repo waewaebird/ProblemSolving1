9c7f9b53672f0c95dab62d049a34ee8aa6c7935b
jzangeva <jzangeva@auto-focus.co.kr>
2024-11-05 16:54:14 +0900

    ~ String 11 and daily test

---
+++ src/String/Main11.java
@@ -0,0 +1,58 @@
+package String;
+
+import java.util.Scanner;
+
+public class Main11 {
+    public static String solution(String s) {
+        String answer = "";
+        int cnt = 1;
+
+        for(int i = 0; i <s.length() ; i++) {
+            if(i > 0) {
+                if(s.charAt(i) != s.charAt(i-1)) {
+                    if(cnt > 1) {
+                        answer += String.valueOf(cnt);
+                        cnt =1;
+                    }
+                    answer += s.charAt(i);
+                } else {
+                    cnt++;
+                }
+
+                if(i == s.length()-1) {
+                    if(cnt > 1) {
+                        answer += String.valueOf(cnt);
+                    }
+                }
+
+            } else {
+                answer += s.charAt(i);
+            }
+        }
+
+
+        //KKHSSSSSSSE K2HS7E
+        //KSTTTSEEKFKKKDJJGG KST3SE2KFK3DJ2G2
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input1 = in.next();
+        System.out.println(solution(input1));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
