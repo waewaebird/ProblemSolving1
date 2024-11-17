baf9129bf4a5d87d363c9abdd6d559577cd563d0
waewaebird <jzangeva@gmail.com>
2024-11-17 23:00:00 +0900

    prior

--- src/Array/Main5.java
+++ src/Array/Main5.java
@@ -1,37 +1,75 @@
 package Array;
 
+import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main5 {
-    public static int solution(int n){
 
-        List<Integer> arr = new ArrayList<>();
-        for(int i = 4 ; i <n+1 ; i++) {
-            arr.add(i);
-        }
 
-        for(int i = 4 ; i <n+1 ; i++) {
-            arr.add(i);
-        }
 
-        for(int i : arr) {
-            if(i%2 == 0) {
-                arr.remove(i);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*public static int solution(int n){
+        int answer = 0;
+        int[] ch = new int[n+1];
+        for(int i=2; i<=n ; i++) {
+            // 2 3 4 5 6 7 8 9 10
+            if(ch[i] == 0) {
+                answer++;
+                for(int j = i ; j<=n; j=j+i) {
+
+                    //2 ; 10 ; 2 4 6 8 10;
+                    //3 ; 10 ; 3 6 9
+
+
+                    ch[j] = 1;
+                }
             }
         }
-
-
-        return arr.size();
-    }
+        return answer;
+    }*/
 
 
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
         int input1 = in.nextInt();
-
         System.out.println(solution(input1));
-
     }
 }
