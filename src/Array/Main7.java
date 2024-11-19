078861c73e857d1a92875135f369ca0bc81d9395
jzangeva <jzangeva@auto-focus.co.kr>
2024-11-19 12:53:30 +0900

    ~ Array 8 , rev

---
+++ src/Array/Main7.java
@@ -0,0 +1,34 @@
+package Array;
+import java.util.Scanner;
+
+public class Main7 {
+    public static int solution(int n, int[] arr) {
+        int answers[] = new int[n];
+        int answer = 0;
+        for(int i = 0 ; i < n ; i++) {
+            if(arr[i] == 0) {
+                answers[i] = 0;
+            } else {
+                if(i == 0) {
+                    answers[i] = 1;
+                } else {
+                    answers[i] = answers[i-1]+1;
+                }
+            }
+            answer = answer + answers[i];
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int input1 = in.nextInt();
+        int[] arr = new int[input1];
+        for(int i = 0 ; i < input1 ; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        int answer = solution(input1, arr);
+        System.out.println(answer);
+    }
+}
---
+++ src/Array/Main8.java
@@ -0,0 +1,37 @@
+package Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main8 {
+    public static List<Integer> solution(int n, int[] arr) {
+        List<Integer> answers = new ArrayList<>();
+        for(int i = 0 ; i < n ; i++) {
+            int cnt = 1;
+
+            for(int j = 0 ; j < n ; j++) {
+                if(i == j) {
+                    continue;
+                } else {
+                    if(arr[i] < arr[j]) {
+                        cnt++;
+                    }
+                }
+            }
+            answers.add(cnt);
+        }
+        return answers;
+    }
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int input1 = in.nextInt();
+        int[] arr = new int[input1];
+        for(int i = 0 ; i < input1 ; i++) {
+            arr[i] = in.nextInt();
+        }
+        for(int nn : solution(input1, arr)) {
+            System.out.print(nn + " ");
+        }
+    }
+}
