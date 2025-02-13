4ab27f23247730ae0e82bbfcf4231716cbfd8201
jzangeva <jzangeva@auto-focus.co.kr>
2025-02-13 09:19:50 +0900

    rev only

---
+++ src/TwoPointersSlidingWindow/rev/Main5RRR.java
@@ -0,0 +1,36 @@
+package TwoPointersSlidingWindow.rev;
+
+import java.util.Scanner;
+
+public class Main5RRR {
+    public static int solution(int n){
+        int answer = 0;
+        int ep = (n/2) + 1;
+
+        int lt = 1;
+        int sum = 0;
+        for (int rt = 1; rt <= ep; rt++) {
+            sum += rt;
+            if(sum >= n) {
+                if(sum == n) {
+                    answer++;
+                }
+
+                while(sum >= n) {
+                    sum = sum-lt;
+                    if(sum == n) {
+                        answer++;
+                    }
+                    lt++;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(solution(n));
+    }
+}
