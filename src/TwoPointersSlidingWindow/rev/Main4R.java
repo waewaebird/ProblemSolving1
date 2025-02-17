cba7665720975fcd6001f6574ae3ece8646a9e6c
jzangeva <jzangeva@auto-focus.co.kr>
2025-02-17 09:19:47 +0900

    ~ Recursive Tree Graph 7 & rev

---
+++ src/TwoPointersSlidingWindow/rev/Main4R.java
@@ -0,0 +1,47 @@
+package TwoPointersSlidingWindow.rev;
+
+import java.util.Scanner;
+
+public class Main4R {
+    public static int solution(int n, int m, int[] arr) {
+        int answer = 0;
+
+        int lt = 0;
+
+        int sum = 0;
+
+        for (int rt = 0; rt < n; rt++) {
+            sum += arr[rt];
+
+            if(sum >= m) {
+                if(sum == m) {
+                    answer++;
+                }
+
+                while(sum >= m){
+                    sum = sum-arr[lt];
+
+                    if(sum == m) {
+                        answer++;
+                    }
+
+                    lt++;
+                }
+            }
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        System.out.println(solution(n,m,arr));
+    }
+}
