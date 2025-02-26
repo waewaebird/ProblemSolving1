54e35c094da4f21dc7d0271c8b94db1464054b29
waewaebird <jzangeva@gmail.com>
2025-02-26 21:38:01 +0900

    ~ DFS BFS 5

--- src/DfsBfs/Main4.java
+++ src/DfsBfs/Main4.java
@@ -3,29 +3,35 @@ package DfsBfs;
 import java.util.Scanner;
 
 public class Main4 {
-    static int[] pm;
     static int n;
     static int m;
+    static int[] arr;
 
-    public static void DFS(int L) {
-        if(L == m) {
-            for(int x : pm) {
+    public static void DFS(int v) {
+        if(v == m) {
+            for (int x : arr) {
                 System.out.print(x + " ");
             }
             System.out.println();
+
         } else {
-            for(int i = 1; i<= n; i++) { //n가닥 호출
-                pm[L] = i;//
-                DFS(L+1);
+            for (int i = 1; i <= n; i++) {
+                arr[v] = i;
+                DFS(v+1);
             }
         }
+
     }
+
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         n = in.nextInt();
         m = in.nextInt();
-        pm = new int[m];
+
+        arr = new int[m];
+
         DFS(0);
+
     }
 }
