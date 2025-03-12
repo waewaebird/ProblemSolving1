c329d69e57ada42f8194eb17a99389c8e59c692c
waewaebird <jzangeva@gmail.com>
2025-03-12 21:47:57 +0900

    ~ DFS BFS 15

--- src/DfsBfs/Main13A.java
+++ src/DfsBfs/Main13A.java
@@ -9,6 +9,24 @@ public class Main13A {
     static int[] dx = {-1,-1,0,1,1,1,0,-1};
     static int[] dy = {0,1,1,1,0,-1,-1,-1};
 
+    Queue<Points> queue = new LinkedList<>();
+    public void BFS(int x, int y, int[][] board) {
+        queue.add(new Points(x,y));
+        while(!queue.isEmpty()) {
+            Points pos = queue.poll();
+
+            for (int i = 0; i < 8; i++) {
+                int nx = pos.x+dx[i];
+                int ny = pos.y+dy[i];
+
+                if(nx>= 0 && nx < n && ny>=0 && ny < n && board[nx][ny] == 1) {
+                    board[nx][ny] = 0;
+                    queue.add(new Points(nx,ny));
+                }
+            }
+        }
+    }
+
     public void DFS(int x, int y, int[][] board) {
         for (int i = 0; i < 8; i++) {
             int nx = x + dx[i];
@@ -29,6 +47,12 @@ public class Main13A {
                     board[i][j] = 0;
 
                     DFS(i,j,board);
+
+                    //answer++;
+                    //board[i][j] = 0;
+                    //T.BFS(i,j,board);
+
+
                 }
             }
         }
