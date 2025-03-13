dead542eb721cbf3a4098a08e3516565ffd19b5e
jzangeva <jzangeva@auto-focus.co.kr>
2025-03-13 16:53:28 +0900

    ~ Greedy Algorithm 2

--- src/GreedyAlgorithm/Main2.java
+++ src/GreedyAlgorithm/Main2.java
@@ -1,8 +1,9 @@
 package GreedyAlgorithm;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
+// 회의가 빨리 끝나는것부터 선택해나간다 !
+// 먼저시작하는걸로 하는것의 반례는 회의시간이 짧은것에 대한 예외가 있을 수 있음
 class Meeting implements Comparable<Meeting> {
     int start;
     int end;
@@ -13,10 +14,10 @@ class Meeting implements Comparable<Meeting> {
 
     @Override
     public int compareTo(Meeting o) {
-        if(this.start == o.start){
-            return this.end - o.end;
+        if(this.end == o.end) {
+            return Integer.compare(this.start,o.start);
         } else {
-            return this.start -o. start;
+            return Integer.compare(this.end,o.end);
         }
     }
 }
@@ -24,28 +25,52 @@ class Meeting implements Comparable<Meeting> {
 public class Main2 {
     static int n;
     static Meeting[] arr;
-    static int temp = 0;
     static int answer = Integer.MIN_VALUE;
-
+    static int temp = 1;
     public static void DFS(int start) {
-        System.out.println(temp);
         answer = Math.max(answer, temp);
 
-        for (int i = start; i < n; i++) {
-            if(arr[start].start <= arr[i].end) {
+        for (int i = start+1; i < n; i++) {
+            if(arr[start].end <= arr[i].start) {
                 temp++;
-                DFS(start+1);
+                DFS(i);
                 temp--;
             }
         }
-
-
     }
 
-    public static void solution(Meeting[] arr) {
-        for (int i = 0; i < n; i++) {
-            DFS(i+1);
+    public static int solution2(List<Meeting> arr , int n) {
+        int cnt = 0;
+        Collections.sort(arr);
+        int et = 0;
+
+        for(Meeting ob : arr) {
+            if(ob.start >= et) {
+                cnt++;
+                et = ob.end;
+            }
         }
+
+        return cnt;
+    }
+
+    public static int solution(List<Meeting> list) {
+        int cnt = 0;
+        int endTime = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+            if(i == 0) {
+                endTime = list.get(i).end;
+                cnt++;
+            } else {
+                if(list.get(i).start >= endTime) {
+                    endTime = list.get(i).end;
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
     }
 
     public static void main(String[] args) {
@@ -55,16 +80,28 @@ public class Main2 {
         
         arr = new Meeting[n];
 
+        List<Meeting> list = new ArrayList<>();
+
         for (int i = 0; i < n; i++) {
             int s = in.nextInt();
             int e = in.nextInt();
 
             arr[i] = new Meeting(s,e);
+            list.add(new Meeting(s,e));
         }
 
-        Arrays.sort(arr);
+        /*Arrays.sort(arr);
 
+        for (int i = 0; i < n; i++) {
+            DFS(i);
+        }
         System.out.println(answer);
+        */
 
+
+        Collections.sort(list);
+
+
+        System.out.println(solution(list));
     }
 }
