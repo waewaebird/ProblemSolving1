126ef5123b20fa9c4a6012ac2eba7e3ca424f9e2
jzangeva <jzangeva@auto-focus.co.kr>
2025-03-20 09:48:06 +0900

    rev only

---
+++ src/RecursiveTreeGraph/rev/Main1RRRR.java
@@ -0,0 +1,23 @@
+package RecursiveTreeGraph.rev;
+
+import java.util.Scanner;
+
+public class Main1RRRR {
+    public static void DFS(int v) {
+        if(v == 0) {
+            return;
+        } else {
+            DFS(v-1);
+            System.out.print(v + " ");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        DFS(n);
+    }
+}
