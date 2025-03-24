8da9a35b7ea2b2c893c349bbf8ee81d583f63ed5
waewaebird <jzangeva@gmail.com>
2025-03-24 21:43:27 +0900

    ~ Dynamic Programming 2

--- src/DynamicProgramming/Main2.java
+++ src/DynamicProgramming/Main2.java
@@ -5,6 +5,16 @@ import java.util.Scanner;
 public class Main2 {
     static int n;
     static int answer;
+    static int[] dy;
+
+    public static int solution(int n) {
+        dy[1] = 1;
+        dy[2] = 2;
+        for (int i = 3; i <= n+1 ; i++) {
+            dy[i] = dy[i-2] + dy[i-1];
+        }
+        return dy[n+1];
+    }
 
     public static void DFS(int v, int sum) {
         if(sum >= n) {
@@ -21,8 +31,11 @@ public class Main2 {
 
         n = in.nextInt();
 
+        dy = new int[n+2];
+
         DFS(1,0);
 
         System.out.println(answer);
+        System.out.println(solution(n));
     }
 }
