7df29775a23453fdd97ecc567bc4db710055fceb
jzangeva <jzangeva@auto-focus.co.kr>
2025-04-24 15:41:26 +0900

    rev

---
+++ src/GreedyAlgorithm/rev/Main8RRRR.java
@@ -0,0 +1,73 @@
+package GreedyAlgorithm.rev;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main8RRRR {
+    static class Wonder implements Comparable<Wonder>{
+        int vex1;
+        int cost;
+
+        public Wonder(int vex1, int cost) {
+            this.vex1 = vex1;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Wonder o) {
+            return Integer.compare(this.cost,o.cost);
+        }
+
+    }
+    static int V;
+    static int E;
+    static List<List<Wonder>> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        V = in.nextInt();
+        E = in.nextInt();
+
+        int[] ch = new int[V+1];
+
+        for (int i = 0; i <=V ; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < E; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int c = in.nextInt();
+
+            list.get(a).add(new Wonder(b,c));
+            list.get(b).add(new Wonder(a,c));
+        }
+
+        PriorityQueue<Wonder> queue = new PriorityQueue<>();
+        queue.offer(new Wonder(1,0));
+
+        // 회로 판단
+
+        int answer = 0;
+        while(!queue.isEmpty()) {
+            Wonder temp = queue.poll();
+
+            if(ch[temp.vex1] == 0) {
+                answer += temp.cost;
+                ch[temp.vex1] = 1;
+
+                for(Wonder w : list.get(temp.vex1)) {
+                    if(ch[w.vex1] == 0) {
+                        queue.offer(new Wonder(w.vex1, w.cost));
+                    }
+                }
+
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
