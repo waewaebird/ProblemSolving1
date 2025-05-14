f5f4f7024ae6623150a67b89a5255147b7675b98
jzangeva <jzangeva@auto-focus.co.kr>
2025-05-14 11:03:11 +0900

    rev & hr

---
+++ src/Array/rev/Main8RRRRRR.java
@@ -0,0 +1,36 @@
+package Array.rev;
+
+import java.util.Scanner;
+
+public class Main8RRRRRR {
+    // 등수구하기
+    public static int[] solution(int n, int[] arr) {
+        int[] answers = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int cnt = 1;
+            for (int j = 0; j < n; j++) {
+                if(arr[i] < arr[j]) {
+                    cnt++;
+                }
+            }
+            answers[i] = cnt;
+        }
+
+        return answers;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        for(int x : solution(n,arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
---
+++ src/zHackerRank/Main_20250514_3.java
@@ -0,0 +1,53 @@
+package zHackerRank;
+
+import java.util.Scanner;
+
+public class Main_20250514_3 {
+    public static int[] solution(int n, int[] arr) {
+        int[] answer = new int[2];
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        int high = 0;
+        int low = 0;
+
+        for (int i = 0; i < n; i++) {
+            if(i == 0) {
+                min = arr[i];
+                max = arr[i];
+            } else {
+                if(arr[i] > max) {
+                    high++;
+                    max = arr[i];
+                }
+
+                if(arr[i] < min) {
+                    low++;
+                    min = arr[i];
+                }
+            }
+        }
+
+        answer[0] = high;
+        answer[1] = low;
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        for(int x: solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+
+    }
+}
