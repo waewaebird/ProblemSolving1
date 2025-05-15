5531704168cbbd661b4173995ac5792cccdc2530
jzangeva <jzangeva@auto-focus.co.kr>
2025-05-15 16:51:06 +0900

    hr

---
+++ src/zHackerRank/Main_20250515_6.java
@@ -0,0 +1,24 @@
+package zHackerRank;
+
+import java.util.Scanner;
+
+public class Main_20250515_6 {
+    // The Hurdle Race
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int k = in.nextInt();
+
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            int a = in.nextInt();
+
+            if(a > k) {
+                answer = Math.max(a-k, answer);
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
