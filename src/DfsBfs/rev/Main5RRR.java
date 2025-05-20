d34fa3fdd7f16987102e00f6a71a1cc64541955c
jzangeva <jzangeva@auto-focus.co.kr>
2025-05-20 09:06:19 +0900

    rev only

---
+++ src/DfsBfs/rev/Main5RRR.java
@@ -0,0 +1,39 @@
+package DfsBfs.rev;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main5RRR {
+    // 동전교환
+    public static int solution(int n, int[] arr, int m) {
+        int dy[] = new int[m+1];
+
+        Arrays.fill(dy, Integer.MAX_VALUE);
+        dy[0] = 0;
+
+        for (int i = 0; i < n ; i++) {
+            for (int j = arr[i]; j <= m; j++) {
+                dy[j] = Math.min(dy[j], dy[j-arr[i]] + 1);
+            }
+        }
+
+        return dy[m];
+    }
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        int m = in.nextInt();
+
+        System.out.println(solution(n,arr,m));
+    }
+}
