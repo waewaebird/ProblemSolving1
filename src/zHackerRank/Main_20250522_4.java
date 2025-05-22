e1e70904b4cbde00cde50f949cc505f50e8beff6
jzangeva <jzangeva@auto-focus.co.kr>
2025-05-22 15:56:55 +0900

    rev & hr

---
+++ src/zHackerRank/Main_20250522_4.java
@@ -0,0 +1,54 @@
+package zHackerRank;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main_20250522_4 {
+    // Cut the sticks
+    public static List<Integer> solution(int n, int[] arr) {
+        List<Integer> answers = new ArrayList<>();
+
+        while(true) {
+            int min = Integer.MAX_VALUE;
+            for (int i = 0; i < n; i++) {
+                if(arr[i] > 0) {
+                    min = Math.min(min, arr[i]);
+                }
+            }
+
+            if(min == Integer.MAX_VALUE) {
+                break;
+            }
+
+            int cnt = 0;
+            for (int i = 0; i < n; i++) {
+                if(arr[i] - min >= 0) {
+                    arr[i] = arr[i] - min;
+                    cnt++;
+                }
+            }
+            answers.add(cnt);
+        }
+
+
+
+        return answers;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        for(int x : solution(n,arr)) {
+            System.out.println(x);
+        }
+    }
+
+}
