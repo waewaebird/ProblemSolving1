f4bcca030426e90ab924e1b72eb1c92b0c8f121b
jzangeva <jzangeva@auto-focus.co.kr>
2025-06-26 08:25:14 +0900

    rev

---
+++ src/DfsBfs/rev/Main9RRRRRRRR.java
@@ -0,0 +1,34 @@
+package DfsBfs.rev;
+
+import java.util.Scanner;
+
+public class Main9RRRRRRRR {
+    // 조합 구하기
+    static int N;
+    static int M;
+    static int[] combi;
+
+    public static void DFS(int v, int start) {
+        if(v == M) {
+            for(int x: combi) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+            return;
+        } else {
+            for (int i = start; i <= N; i++) {
+                combi[v] = i;
+                DFS(v+1, i+1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        N = in.nextInt();
+        M = in.nextInt();
+        combi = new int[M];
+
+        DFS(0,1);
+    }
+}
