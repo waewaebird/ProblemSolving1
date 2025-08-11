5946e349606dadd64a43d9ffbbaed7467d57deab
jzangeva <jzangeva@auto-focus.co.kr>
2025-08-11 13:18:23 +0900

    rev & hring

---
+++ src/String/rev/Main1RRRRRRRRRR.java
@@ -0,0 +1,26 @@
+package String.rev;
+
+import java.util.Scanner;
+
+public class Main1RRRRRRRRRR {
+    // 문자 찾기
+    public static Integer solution(String str, String s) {
+        int answer = 0;
+        for(char x : str.toLowerCase().toCharArray()) {
+            if(x == s.toLowerCase().charAt(0)) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String str = in.next();
+        String s = in.next();
+
+        System.out.println(solution(str,s));
+    }
+}
