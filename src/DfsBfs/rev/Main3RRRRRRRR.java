1a1a29732fcb4ef11250a56d5c3444d3190db10b
jzangeva <jzangeva@auto-focus.co.kr>
2025-08-21 11:14:04 +0900

    rev & hr

---
+++ src/DfsBfs/rev/Main3RRRRRRRR.java
@@ -0,0 +1,56 @@
+package DfsBfs.rev;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main3RRRRRRRR {
+
+    static class Test {
+        int point;
+        int time;
+
+        public Test(int point, int time) {
+            this.point = point;
+            this.time = time;
+        }
+    }
+
+    static int N;
+    static int M;
+    static List<Test> list = new ArrayList<>();
+    static int answer = 0;
+
+    public static void DFS(int v, int tsum, int psum) {
+        if(tsum > M) {
+            return;
+        }
+
+        answer = Math.max(answer,psum);
+
+        if(v == N) {
+            return;
+        } else {
+            DFS(v+1, tsum + list.get(v).time, psum + list.get(v).point);
+            DFS(v+1, tsum, psum);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        N = in.nextInt();
+        M = in.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+
+            list.add(new Test(a,b));
+        }
+
+        DFS(0,0, 0);
+        System.out.println(answer);
+    }
+
+}
