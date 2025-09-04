a9adff188e48aa9a7f3c3df0a112698ad70a32c4
jzangeva <jzangeva@auto-focus.co.kr>
2025-09-04 16:02:27 +0900

    pr

---
+++ src/zProgrammers/Solution_20250904_2.java
@@ -0,0 +1,37 @@
+package zProgrammers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution_20250904_2 {
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+
+        Map<String, Integer> wordMap = new HashMap<>();
+
+        for (int i = 0; i < words.length; i++) {
+            if(i == 0) {
+                wordMap.put(words[i], 1);
+            } else {
+                if(words[i].charAt(0) != words[i-1].charAt(words[i-1].length()-1)) {
+                    int p = i % n + 1;
+                    int c = (i) / n + 1;
+                    answer[0] = p;
+                    answer[1] = c;
+                    break;
+                } else {
+                    if(wordMap.get(words[i]) != null) {
+                        int p = i % n + 1;
+                        int c = (i) / n + 1;
+                        answer[0] = p;
+                        answer[1] = c;
+                        break;
+                    } else {
+                        wordMap.put(words[i], 1);
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
