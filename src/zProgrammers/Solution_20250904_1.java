76678b6abe8c93cc2d2a3420647e948383e3797e
jzangeva <jzangeva@auto-focus.co.kr>
2025-09-04 16:02:27 +0900

    pr

---
+++ src/zProgrammers/Solution_20250904_1.java
@@ -0,0 +1,29 @@
+package zProgrammers;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution_20250904_1 {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        Deque<Character> stack = new ArrayDeque<>();
+        for(char x : s.toCharArray()) {
+            if(x == '(') {
+                stack.push(x);
+            } else {
+                if(stack.size() > 0) {
+                    stack.pop();
+                } else {
+                    answer = false;
+                    break;
+                }
+            }
+        }
+
+        if(stack.size() > 0) {
+            answer = false;
+        }
+        return answer;
+    }
+}
