1a7ae2ef6fccdca3872481248f4958315b0eebf6
jzangeva <jzangeva@auto-focus.co.kr>
2025-09-10 12:57:12 +0900

    rev & pr

---
+++ src/DfsBfs/rev/Main7RRRRRRRRRRRRRR.java
@@ -0,0 +1,36 @@
+package DfsBfs.rev;
+
+import java.util.Scanner;
+
+public class Main7RRRRRRRRRRRRRR {
+    // 조합의 경우수(메모이제이션)
+    static int[][] memo;
+
+    public static Integer DFS(int n, int r) {
+        if(memo[n][r] > 0) {
+            return memo[n][r];
+        }
+
+        if(n == r) {
+            return memo[n][r] = 1;
+        }
+
+        if(r == 1) {
+            return memo[n][r] = n;
+        }
+
+        return memo[n][r] = DFS(n-1, r-1) + DFS(n-1, r);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int r = in.nextInt();
+
+        memo = new int[n+1][r+1];
+
+        System.out.println(DFS(n,r));
+
+    }
+}
---
+++ src/DynamicProgramming/rev/Main4RRRRRR.java
@@ -0,0 +1,65 @@
+package DynamicProgramming.rev;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main4RRRRRR {
+    static class Brick implements Comparable<Brick>{
+        int width;
+        int height;
+        int weight;
+
+        public Brick(int width, int height, int weight) {
+            this.width = width;
+            this.height = height;
+            this.weight = weight;
+        }
+
+
+        @Override
+        public int compareTo(Brick o) {
+            return Integer.compare(this.width, o.width);
+        }
+    }
+
+    public static Integer solution(int n, List<Brick> list) {
+        int answer = 0;
+        list.sort(null);
+
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = list.get(i).height;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = i-1; j >= 0 ; j--) {
+                if(list.get(i).weight > list.get(j).weight) {
+                    dp[i] = Math.max(dp[i], dp[j] + list.get(i).height);
+                }
+            }
+            answer = Math.max(answer, dp[i]);
+        }
+
+        //System.out.println(Arrays.toString(dp));
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        List<Brick> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int c = in.nextInt();
+
+            list.add(new Brick(a,b,c));
+        }
+
+        System.out.println(solution(n,list));
+    }
+}
