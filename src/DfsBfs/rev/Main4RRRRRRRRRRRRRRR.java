b6bbd1c2546c1f4dd074d53086cdc05b527cb314
jzangeva <jzangeva@auto-focus.co.kr>
2025-09-24 10:20:08 +0900

    rev & pr

---
+++ src/DfsBfs/rev/Main4RRRRRRRRRRRRRRR.java
@@ -0,0 +1,34 @@
+package DfsBfs.rev;
+
+import java.util.Scanner;
+
+public class Main4RRRRRRRRRRRRRRR {
+    // 중복순열 구하기
+    static int N;
+    static int M;
+    static int[] arr;
+
+    public static void DFS(int v) {
+        if(v == M) {
+            for(int x : arr) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        } else {
+            for (int i = 1; i <= N; i++) {
+                arr[v] = i;
+                DFS(v+1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        N = in.nextInt();
+        M = in.nextInt();
+
+        arr = new int[M];
+
+        DFS(0);
+    }
+}
---
+++ src/GreedyAlgorithm/rev/Main3RRRRRRRRR.java
@@ -0,0 +1,61 @@
+package GreedyAlgorithm.rev;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main3RRRRRRRRR {
+    // 결혼식
+    static class Wedding implements Comparable<Wedding> {
+        int time;
+        char word;
+
+        public Wedding(int time, char word) {
+            this.time = time;
+            this.word = word;
+        }
+
+        @Override
+        public int compareTo(Wedding o) {
+            if(this.time == o.time) {
+                return Integer.compare(o.word, this.word);
+            } else{
+                return Integer.compare(this.time, o.time);
+            }
+        }
+    }
+
+    public static Integer solution(int n, List<Wedding> list) {
+        list.sort(null);
+
+        int answer = 0;
+
+        int cnt = 0;
+        for(Wedding w : list) {
+            if(w.word == 'a') {
+                cnt++;
+            } else {
+                cnt--;
+            }
+            answer = Math.max(answer,cnt);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        List<Wedding> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+
+            list.add(new Wedding(a, 'a'));
+            list.add(new Wedding(b, 'b'));
+        }
+
+        System.out.println(solution(n,list));
+    }
+}
