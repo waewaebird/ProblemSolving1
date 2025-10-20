f50e0a8687dfe0298697683ae90ebed1fd40c46e
jzangeva <jzangeva@auto-focus.co.kr>
2025-10-20 09:02:04 +0900

    rev & pr

---
+++ src/zProgrammers/Solution_20251020_1.java
@@ -0,0 +1,19 @@
+package zProgrammers;
+
+class Solution_20251020_1 {
+    public int solution(int[] num_list) {
+        String odd = "";
+        String even = "";
+
+        for(int x : num_list) {
+            if(x % 2 == 0) {
+                even += String.valueOf(x);
+            } else {
+                odd += String.valueOf(x);
+            }
+        }
+
+        int answer = Integer.valueOf(odd) + Integer.valueOf(even);
+        return answer;
+    }
+}
