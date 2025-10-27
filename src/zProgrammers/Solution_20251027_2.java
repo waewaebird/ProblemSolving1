d20acc335828a9df990eac4551412f35f6d2ff5e
jzangeva <jzangeva@auto-focus.co.kr>
2025-10-27 09:29:20 +0900

    rev & pr

---
+++ src/zProgrammers/Solution_20251027_2.java
@@ -0,0 +1,20 @@
+package zProgrammers;
+
+import java.math.BigInteger;
+
+
+class Solution_20251027_2 {
+    public int solution(int n) {
+
+        BigInteger[] arr = new BigInteger[n];
+
+        arr[0] = BigInteger.ONE;
+        arr[1] = BigInteger.ONE;
+
+        for (int i = 2; i < n; i++) {
+            arr[i] = arr[i-2].add(arr[i-1]);
+        }
+
+        return Integer.valueOf(String.valueOf(arr[n-1].remainder(BigInteger.valueOf(1234567))));
+    }
+}
