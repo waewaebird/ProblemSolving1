634c20326f76d0a362990bcdee0f12a2938a3eed
jzangeva <jzangeva@auto-focus.co.kr>
2025-11-14 12:46:48 +0900

    rev & pr

---
+++ src/DynamicProgramming/rev/Main1RRRRRRRRRRR.java
@@ -0,0 +1,27 @@
+package DynamicProgramming.rev;
+
+import java.util.Scanner;
+
+public class Main1RRRRRRRRRRR {
+    // 계단오르기
+    public static int solution(int n) {
+        int[] dp = new int[n+1];
+
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        dp[4] = 5;
+
+        for (int i = 5; i <= n; i++) {
+            dp[i] = dp[i-2] + dp[i-1];
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(solution(n));
+    }
+}
---
+++ src/TwoPointersSlidingWindow/rev/Main4RRRR.java
@@ -0,0 +1,49 @@
+package TwoPointersSlidingWindow.rev;
+
+import java.util.Scanner;
+
+public class Main4RRRR {
+    // 연속 부분수열
+    public static int solution(int n, int m, int[] arr) {
+        int answer = 0;
+
+        int sum = 0;
+        int lt = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+
+            if(sum >= m) {
+                if(sum == m) {
+                    answer++;
+                }
+
+                while(sum >= m) {
+                    sum -= arr[lt];
+
+                    if(sum == m) {
+                        answer++;
+                    }
+
+                    lt++;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        System.out.println(solution(n,m,arr));
+    }
+
+}
