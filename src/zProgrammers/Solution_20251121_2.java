b9463b41cbc05b5476e04a5de358fdc9e7fc8352
jzangeva <jzangeva@auto-focus.co.kr>
2025-11-21 13:53:12 +0900

    rev & pr

---
+++ src/zProgrammers/Solution_20251121_2.java
@@ -0,0 +1,20 @@
+package zProgrammers;
+
+class Solution_20251121_2 {
+    public int[] solution(long n) {
+        int len = String.valueOf(n).length();
+
+        int[] answer = new int[len];
+
+        String str = String.valueOf(n);
+        for (int i = len-1; i >= 0 ; i--) {
+            answer[Math.abs(i - len) - 1] = Integer.valueOf(String.valueOf(str.charAt(i)));
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution_20251121_2 s = new Solution_20251121_2();
+        System.out.println(s.solution(12345));
+    }
+}
