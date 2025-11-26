bf8eff5c40b362e8d69446198e4c246dae34310d
jzangeva <jzangeva@auto-focus.co.kr>
2025-11-26 09:55:10 +0900

    rev & pr

---
+++ src/GreedyAlgorithm/rev/Main3RRRRRRRRRRRRR.java
@@ -0,0 +1,60 @@
+package GreedyAlgorithm.rev;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main3RRRRRRRRRRRRR {
+    // 결혼식
+    static class Marry implements Comparable<Marry> {
+        int time;
+        char word;
+
+        public Marry(int time, char word) {
+            this.time = time;
+            this.word = word;
+        }
+
+        @Override
+        public int compareTo(Marry o) {
+            if(this.time == o.time) {
+                return Integer.compare(o.word, this.word);
+            } else {
+                return Integer.compare(this.time, o.time);
+            }
+        }
+    }
+
+    public static int solution(int n, List<Marry> list) {
+        list.sort(null);
+
+        int answer = 0;
+        int max = 0;
+        for(Marry m : list) {
+            if(m.word == 'a') {
+                max++;
+            } else {
+                max--;
+            }
+            answer = Math.max(answer, max);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        List<Marry> list = new ArrayList<>();
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+
+            list.add(new Marry(a,'a'));
+            list.add(new Marry(b,'b'));
+        }
+
+        System.out.println(solution(n,list));
+    }
+}
---
+++ src/zProgrammers/Solution_20251126_3.java
@@ -0,0 +1,27 @@
+package zProgrammers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution_20251126_3 {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        int len = answer.length;
+        for (int i = 0; i < len; i++) {
+            int[] temp = commands[i];
+
+            List<Integer> list = new ArrayList<>();
+            for (int j = 1; j <= array.length; j++) {
+                if(j >= temp[0] && j <= temp[1]) {
+                    list.add(array[j-1]);
+                }
+            }
+            list.sort(null);
+
+            answer[i] = list.get(temp[2] -1);
+        }
+
+        return answer;
+    }
+}
