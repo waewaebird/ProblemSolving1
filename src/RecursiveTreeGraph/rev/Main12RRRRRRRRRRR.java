e240cd4a114f8d5918a0d7b428374a4a4acc1714
jzangeva <jzangeva@auto-focus.co.kr>
2025-11-28 10:45:54 +0900

    rev & pr

---
+++ src/RecursiveTreeGraph/rev/Main12RRRRRRRRRRR.java
@@ -0,0 +1,52 @@
+package RecursiveTreeGraph.rev;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main12RRRRRRRRRRR {
+    // 경로탐색(인접행렬)
+    static int N;
+    static int M;
+    static List<List<Integer>> list = new ArrayList<>();
+    static int[] ch;
+    static int answer = 0;
+
+    public static void DFS(int v) {
+        if(v == N) {
+            answer++;
+        } else {
+            for(int x : list.get(v)) {
+                if(ch[x] == 0) {
+                    ch[x] = 1;
+                    DFS(x);
+                    ch[x] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        N = in.nextInt();
+        M = in.nextInt();
+
+        ch = new int[N+1];
+        for (int i = 0; i <= N; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+
+            list.get(a).add(b);
+        }
+
+        ch[1] = 1;
+        DFS(1);
+
+        System.out.println(answer);
+    }
+}
---
+++ src/zProgrammers/Solution_20251128_1.java
@@ -0,0 +1,13 @@
+package zProgrammers;
+
+class Solution_20251128_1 {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+
+        for (int i = 0; i < n; i++) {
+            answer[i] = (long)x * (long)(i+1);
+        }
+
+        return answer;
+    }
+}
