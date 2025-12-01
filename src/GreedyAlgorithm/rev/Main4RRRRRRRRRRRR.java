57995f405bf34eb7d78fe86541d7553c70699286
jzangeva <jzangeva@auto-focus.co.kr>
2025-12-01 09:34:15 +0900

    rev & pr

---
+++ src/GreedyAlgorithm/rev/Main4RRRRRRRRRRRR.java
@@ -0,0 +1,64 @@
+package GreedyAlgorithm.rev;
+
+import java.util.*;
+
+public class Main4RRRRRRRRRRRR {
+    // 최대 수입 스케쥴
+    static class Lecture implements Comparable<Lecture> {
+        int money;
+        int day;
+
+        public Lecture(int money, int day) {
+            this.money = money;
+            this.day = day;
+        }
+
+        @Override
+        public int compareTo(Lecture o) {
+            return Integer.compare(o.day, this.day);
+        }
+    }
+
+    public static int solution(List<Lecture> list) {
+        int answer = 0;
+
+        list.sort(null);
+        int max = list.get(0).day;
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        int j = 0;
+        for (int i = max; i >= 1; i--) {
+            for (; j < list.size(); j++) {
+                if(list.get(j).day == i) {
+                    pq.offer(list.get(j).money);
+                } else if(list.get(j).day < i){
+                    break;
+                }
+            }
+            if(!pq.isEmpty()) {
+                answer += pq.poll();
+            }
+        }
+
+        return answer;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        List<Lecture> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+
+            list.add(new Lecture(a,b));
+        }
+        System.out.println(solution(list));
+    }
+}
