38996299f72cdbeae0237486518a964949f98b72
jzangeva <jzangeva@auto-focus.co.kr>
2025-12-02 12:59:32 +0900

    rev & pr

---
+++ src/zProgrammers/Solution_20251202_2.java
@@ -0,0 +1,13 @@
+package zProgrammers;
+
+class Solution_20251202_2 {
+    public int solution(int number, int n, int m) {
+        int answer = 0;
+
+        if(number % n == 0 && number % m == 0) {
+            answer = 1;
+        }
+
+        return answer;
+    }
+}
