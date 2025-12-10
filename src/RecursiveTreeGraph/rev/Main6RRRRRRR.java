67494e180bc7dcd79144595f2b7f42564a965d45
jzangeva <jzangeva@auto-focus.co.kr>
2025-12-10 12:32:50 +0900

    rev & pr

---
+++ src/RecursiveTreeGraph/rev/Main6RRRRRRR.java
@@ -0,0 +1,37 @@
+package RecursiveTreeGraph.rev;
+
+import java.util.Scanner;
+
+public class Main6RRRRRRR {
+    // 부분집합 구하기(DFS)
+
+    static int N;
+    static int[] ch;
+
+    public static void DFS(int v) {
+        if(v == N+1) {
+            for (int i = 1; i <= N; i++) {
+                if(ch[i] == 1) {
+                    System.out.print(i + " ");
+                }
+            }
+            System.out.println();
+        } else {
+            if(ch[v] == 0) {
+                ch[v] = 1;
+                DFS(v+1);
+                ch[v] = 0;
+                DFS(v+1);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        N = in.nextInt();
+        ch = new int[N+1];
+
+        DFS(1);
+    }
+}
---
+++ src/zProgrammers/Solution_20251210_2.java
@@ -0,0 +1,40 @@
+package zProgrammers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution_20251210_2 {
+    public String solution(String s, String skip, int index) {
+        String answer = "";
+        Map<Integer, Boolean> map = new HashMap<>();
+        for(char x : skip.toCharArray()) {
+            int temp = (int) x;
+            map.put(temp, true);
+        }
+
+        // a == 97
+        // z == 122
+
+        for(char x : s.toCharArray()) {
+            int now = (int) x;
+            int next = now;
+
+            int cnt = 0;
+            while(cnt < index) {
+                next++;
+
+                if(next > 122) {
+                    next = 97;
+                }
+
+                if(map.get(next) == null) {
+                    cnt++;
+                }
+            }
+
+            answer += (char) next;
+        }
+
+        return answer;
+    }
+}
