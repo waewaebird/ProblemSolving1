d716dcc9154fb3f4d37647c52f6fefa1988b193f
jzangeva <jzangeva@auto-focus.co.kr>
2025-12-10 12:32:50 +0900

    rev & pr

---
+++ src/Array/rev/Main7RRRRRRRRRR.java
@@ -0,0 +1,36 @@
+package Array.rev;
+
+import java.util.Scanner;
+
+public class Main7RRRRRRRRRR {
+    // 점수계산
+    public static int solution(int n, int[] arr) {
+        int answer = 0;
+
+        int cnt = 1;
+
+        for (int i = 0; i < n; i++) {
+            if(arr[i] == 1) {
+                answer += 1*cnt;
+                cnt++;
+            } else {
+                cnt = 1;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        System.out.println(solution(n,arr));
+    }
+}
