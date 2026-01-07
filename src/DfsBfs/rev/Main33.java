d8cdebd45de82e3f8551d3676a73a6f952c858cc
jzangeva <jzangeva@auto-focus.co.kr>
2026-01-07 10:19:32 +0900

    rev & pr

---
+++ src/DfsBfs/rev/Main33.java
@@ -0,0 +1,44 @@
+package DfsBfs.rev;
+
+import java.util.Scanner;
+
+public class Main33 {
+    // 미로탐색
+    static int[][] arr = new int[7][7];
+    static int[] DI = {-1,0,0,1};
+    static int[] DJ = {0,-1,1,0};
+    static int answer = 0;
+
+    public static void DFS(int v1,int v2){
+        if(v1 == 6 && v2 == 6){
+            answer++;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int n1 = v1 + DI[i];
+            int n2 = v2 + DJ[i];
+
+            if(n1 >= 0 && n1 < 7 && n2 >= 0 && n2 < 7) {
+                if(arr[n1][n2] == 0) {
+                    arr[n1][n2] = 1;
+                    DFS(n1,n2);
+                    arr[n1][n2] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                arr[i][j] = in.nextInt();
+            }
+        }
+
+        arr[0][0] = 1;
+        DFS(0,0);
+        System.out.println(answer);
+    }
+}
