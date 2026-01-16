3dc62031cbc6b3a12617330a76a17cdb2c81df83
jzangeva <jzangeva@auto-focus.co.kr>
2026-01-16 16:30:59 +0900

    rev & pr

---
+++ src/RecursiveTreeGraph/rev/Main4RRRRRR.java
@@ -0,0 +1,29 @@
+package RecursiveTreeGraph.rev;
+
+import java.util.Scanner;
+
+public class Main4RRRRRR {
+    // 피보나치 수열
+    public static int[] solution(int n) {
+        int[] arr = new int[n];
+        arr[0] = 1;
+        arr[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            arr[i] = arr[i-2] + arr[i-1];
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        for(int x : solution(n)) {
+            System.out.print(x + " ");
+        }
+
+    }
+}
